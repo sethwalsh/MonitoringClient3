dca1b877271a47c1146b2a64a0a2df92cbc9ccdb
Seth Walsh <seth.walsh@utah.edu>
2017-03-09 15:08:19 -0700

    added service project installer

--- MonitoringClientService/Service1.cs
+++ MonitoringClientService/Service1.cs
@@ -8,6 +8,8 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+
 namespace MonitoringClientService
 {
     public partial class Service1 : ServiceBase
@@ -20,10 +22,24 @@ namespace MonitoringClientService
         protected override void OnStart(string[] args)
         {
             /// For now just create a new Client
+            BackgroundWorker bw_ = new BackgroundWorker();
+            bw_.DoWork += new DoWorkEventHandler(workerThreadFunction);
+            bw_.RunWorkerAsync();
         }
 
         protected override void OnStop()
         {
+            p.Kill();
         }
+
+        private void workerThreadFunction(object sender, DoWorkEventArgs e)
+        {
+            p = new Process();
+            p.StartInfo = new ProcessStartInfo("C:\\Tools\\Monitoring\\MonitoringClient.exe");
+            p.WaitForExit();
+            base.Stop();
+        }
+
+        private Process p;
     }
 }
